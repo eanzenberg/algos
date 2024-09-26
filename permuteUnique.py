eadfaa586802f528c6ac8fe241fa69d43a611ea3
eitan <eanzenberg@icloud.com>
2024-09-25 21:29:29 -0700

    .

--- permuteUnique.py
+++ permuteUnique.py
@@ -1,26 +1,29 @@
 from typing import List
+from collections import defaultdict
 
 
 def permuteUnique(nums: List[int]) -> List[List[int]]:
-
-    def permute(nums: List[int]):
-        if len(nums) == 1:
-            return [nums]
+    def permute(nums: List[int]) -> List[List[int]]:
+        permutations = defaultdict(list)        
         
-        permutations = []
-
+        if len(nums) == 1:
+            permutations[tuple(nums)] = None
+            return permutations
+        
         for i in range(len(nums)):
             item = nums[i]
             remaining_list = nums[:i] + nums[i+1:]
 
             for remaining_items in permute(remaining_list):
                 key = [item] + list(remaining_items)
-                permutations.append(key)
+                if tuple(key) not in permutations:
+                    permutations[tuple(key)] = None
 
         return permutations
 
-    return [list(i) for i in set(tuple(i) for i in permute(nums))]
-    
+    return [list(i) for i in permute(nums).keys()]
+
 
 print(permuteUnique([1,2,3]), [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
 print(permuteUnique([1,1,2]), [[1,1,2],[1,2,1],[2,1,1]])
+print(permuteUnique([1]), [[1]])
